1fb2e55d3688cce5935b255b8edb38f30660e824
Fraideron <jiafen@yandex.ua>
2017-02-21 16:27:13 +0200

    update documents in colection, need some debug

--- app.js
+++ app.js
@@ -117,7 +117,6 @@ var server = app.listen(8082, function () {
 
     var host = server.address().address;
     var port = server.address().port;
-
     console.log("Example app listening at http://%s:%s", host, port)
 
 })
