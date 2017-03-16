b4762bb3f29757970e1c13fe9db3e27a1721e1e0
Vlad Dotsenko <vladislav.dotsenko@onix-systems.com>
2017-03-17 01:32:44 +0200

    Routes sanity unit tests improoved, depndencies fixed

--- tests/routes-quicktest.js
+++ tests/routes-quicktest.js
@@ -8,8 +8,9 @@ describe('Routes sanity test', () => {
 
 	Object.keys(routes).forEach(r => {
 		const _spl = r.split(' ', 2);
-		it('respond with json', done => {
-			request(app)[_spl[0]](_spl[1])
+    const route = _spl[1].replace(/:(.*?)(\/|$)/, '1$2')
+    it(`'${_spl[0]} ${route}' respond with json`, done => {
+			request(app)[_spl[0]](route)
 			.set('Accept', 'application/json')
 			.expect('Content-Type', /json/)
 			.expect(200, done);
