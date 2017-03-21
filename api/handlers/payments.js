e2fc3d7b190bdf414a4964e66b58c988dbbbda91
Fraideron <jiafen24@gmail.com>
2017-03-21 12:59:30 +0200

    add 'get' and 'get by type' to payments

--- api/handlers/payments.js
+++ api/handlers/payments.js
@@ -1,14 +1,20 @@
 /**
  * Created by valeriy on 03.03.17.
  */
-
+'use strict';
+let storage = require('./../storage');
 module.exports = {
     GET: function (req, res, next) {
-        res.send('get payments');
+        res.json(storage.users.data.payments);
     },
 
     getByType: function (req, res, next) {
-        res.send('get payments');
+        let filter = req.params.type;
+        let payments = storage.users.data.payments[0].bill;
+        let result = payments.filter(function (element) {
+            return(element.type === filter);
+        })
+        res.json(result);
     },
 
     put: function (req, res, next) {
--- api/handlers/taxes.js
+++ api/handlers/taxes.js
@@ -1,10 +1,12 @@
 /**
  * Created by valeriy on 03.03.17.
  */
+'use strict';
+let storage = require('./../storage');
 
 module.exports={
     GET: function (req, res, next) {
-
+        res.json(storage.users.data.taxes);
     },
 
     put: function (req, res, next) {
