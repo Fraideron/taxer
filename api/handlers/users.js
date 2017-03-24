aa267de9732f817105066cb731839ad88bb579a8
Fraideron <jiafen24@gmail.com>
2017-03-24 10:11:34 +0200

    created users handler

---
+++ api/handlers/users.js
@@ -0,0 +1,18 @@
+/**
+ * Created by valeriy on 23.03.17.
+ */
+'use strict';
+
+module.exports = {
+    GET: function (req, res, next) {
+
+    },
+
+    postProfile: function (req, res, next) {
+
+    },
+
+    postMeta: function (req, res, next) {
+
+    }
+}
