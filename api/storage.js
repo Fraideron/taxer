eec535761e40eb4fd6f738d365d1c70a2fed3030
Fraideron <jiafen24@gmail.com>
2017-04-05 15:31:28 +0300

    storage changes

--- api/storage.js
+++ api/storage.js
@@ -2,53 +2,12 @@
  * Created by valeriy on 20.03.17.
  */
 module.exports = {
-    users: {
-        "_id" : 'ObjectId("58b5b9b1c3bdb025cf8c4d06")',
-        "login" : [],
-        "data" : {
-            "payments" : [
-                {
-                    "date" : "20170218",
-                    "bill" : [
-                        {
-                            "type" : "gas",
-                            "expected" : 123,
-                            "payed" : 125
-                        },
-                        {
-                            "type" : "electricity",
-                            "payed" : 125
-                        }
-                    ]
-                }
-            ],
-            "wastes" : [
-                {
-                    "type" : "gas",
-                    "value" : 123,
-                    "date" : "2017-02-18"
-                },
-                {
-                    "type" : "oil",
-                    "value" : 133,
-                    "date" : "2017-03-18",
-                    "payed" : 40,
-                    "rate" : 4
-                },
-                {
-                    "type" : "electric",
-                    "value" : 143,
-                    "date" : "2017-04-18",
-                    "payed" : 0,
-                    "rate" : 0
-                }
-            ],
-            "taxes" : [
-                {
-                    "type": "gas",
+   var MongoClient = require('mongodb').MongoClient;
 
-                }
-            ]
+// Connect to the db
+    MongoClient.connect("mongodb://localhost:27017/taxer", function(err, db) {
+        if(!err) {
+            console.log("We are connected");
         }
-    }
+    });
 };
