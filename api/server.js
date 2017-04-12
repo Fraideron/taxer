832030022f8f5bcf08ab944e640b43fdad20b025
Fraideron <jiafen24@gmail.com>
2017-04-12 13:01:35 +0300

    resolve typos

--- api/server.js
+++ api/server.js
@@ -14,7 +14,6 @@ const assert = require('assert');
 
 let collections = ['users', 'wastes'];
 let model = {};
-let userID = new mongo.ObjectID('58e4e4b2bb31a671885209a6');
 
 MongoClient.connect("mongodb://localhost:27017/taxer", function(err, db) {
     if(!err) {
@@ -40,42 +39,30 @@ app.use(function (req, res, next) {
     next();
 });
 
-
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-// @todo:
-// What does this function do?
-// Does it makes url with environment?
-// This name is too long and bring no fucking info about what is it doing
-function makeUrlWitnEnviroment(prefix, routes) {
-  for (let rout in routes) {
-    const parsUrl = parseUrl(prefix, rout);
-    // @todo: WTF is this? Bunyan? Really?
-    console.log(`Method: ${parsUrl.method} URL:${parsUrl.url} \n function: ${routes[rout]}`);
-    app[parsUrl.method](parsUrl.url, routes[rout]);
-  }
+function applyRoutes(prefix, routes) {
+    for (let route in routes) {
+        const parsUrl = parseUrl(prefix, route);
+        app[parsUrl.method](parsUrl.url, routes[route]);
+        log.info(parsUrl, 'Listening for route');
+    }
 }
 
-makeUrlWitnEnviroment('api', require('./routes').api);
-// @todo:
-// learn english a bit
-// Words order is inverce, should be 'evironmentType'
-let typeEnviroment = appConfigs.environment;
-if (typeEnviroment == 'test') {
-  makeUrlWitnEnviroment(typeEnviroment, require('./routes').test);
+applyRoutes('api', require('./routes').api);
+let evironmentType = appConfigs.environment;
+if (evironmentType == 'test') {
+    applyRoutes(typeEnviroment, require('./routes').test);
 }
 
 // uncaught errors handler
 app.use(function(err, req, res, next) {
-  log.warn('Something is broken!');
-  // @todo: WTF is this? Bunyan? Really?
-  console.error(err.stack);
-  res.status(500).send('Something is broken!');
+    log.warn(err.stack, 'Something is broken!');
+    res.status(500).send('Something is broken!');
 });
 
 module.exports = app;
